22eccaf6d33685286729a93f7f7f339448cd4c39
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2026-08-08 19:00:30 +0200

    Relicense project under EPL-2.0 with GPL Classpath exception

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject de.dixieflatline/paperboy "0.1.0-SNAPSHOT"
   :description "Reliable message passing to arbitrary brokers"
   :url "http://github.com/20centaurixfux/paperboy"
-  :license {:name "AGPLv3.0"
-            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]])
